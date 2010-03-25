5ea2c0aec79aadc8853459ecf0660102526759f8
Lincoln Stoll <lstoll@lstoll.net>
2010-03-25 17:11:41 +0800

    Server listens on TCP as well on macbook.

--- linc.local.el
+++ linc.local.el
@@ -1,3 +1,6 @@
+; Listen on TCP
+(setq server-use-tcp t)
+(setq server-host "127.0.0.1")
 ; Machine specific - start the server, blow away the socket if it errors and retry
 (condition-case nil
   (server-start)
