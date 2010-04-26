fcdd583b7027e4bf7280b907f62096a574f91640
Lincoln Stoll <lstoll@lstoll.net>
2010-04-26 04:08:56 +0200

    Make browser config system specific, use default os x browser on macbook

--- linc.local.el
+++ linc.local.el
@@ -8,3 +8,14 @@
     (call-process "/bin/sh" nil nil nil "-c" "find /var/folders -name server 2>/dev/null | grep emacs501 | xargs rm")
     (server-start))
   )
+
+;; Use default OS X Browser
+(defun rcy-browse-url-default-macosx-browser (url &optional new-window)
+    (interactive (browse-url-interactive-arg "URL: "))
+      (let ((url
+                 (if (aref (url-generic-parse-url url) 0)
+                         url
+                             (concat "http://" url))))
+            (start-process (concat "open " url) nil "open" url)))
+ 
+(setq browse-url-browser-function 'rcy-browse-url-default-macosx-browser)
