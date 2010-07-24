f3b955d23b3860cadf6ce286d3addc574e34a47a
Lincoln Stoll <l@lds.li>
2010-07-24 23:13:08 +0200

    .js is always js2-mode, enable flymake automatically

--- conf/js.el
+++ conf/js.el
@@ -135,7 +135,37 @@
 
 (add-hook 'js2-mode-hook 'my-js2-mode-hook)
 
+;; Flymake with JSLint
 
+(require 'flymake)
+
+(defun flymake-jslint-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+		     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+		      temp-file
+		      (file-name-directory buffer-file-name))))
+    (list "jsl" (list "-process" local-file))))
+
+(setq flymake-allowed-file-name-masks
+      (cons '(".+\\.js$"
+	      flymake-jslint-init
+	      flymake-simple-cleanup
+	      flymake-get-real-file-name)
+	    flymake-allowed-file-name-masks))
+
+(setq flymake-err-line-patterns 
+      (cons '("^Lint at line \\([[:digit:]]+\\) character \\([[:digit:]]+\\): \\(.+\\)$"  
+	      nil 1 2 3)
+	    flymake-err-line-patterns))
+
+; Enable it automatically
+(add-hook 'javascript-mode-hook
+          (lambda () (flymake-mode 1)))
+(add-hook 'js2-mode-hook
+          (lambda () (flymake-mode 1)))
+
+;(setq flymake-log-level 3)
 
 (provide 'conf/js)
 ;;; starter-kit-js.el ends here
