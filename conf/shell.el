75ed7a1ff223a285190610f1d8b372a6a31e0c0f
Lincoln Stoll <lstoll@lstoll.net>
2012-03-14 11:23:54 +1100

    Try term instead of shell

--- conf/shell.el
+++ conf/shell.el
@@ -1,9 +1,9 @@
-(add-hook 'shell-mode-hook (lambda()
+(add-hook 'term-mode-hook (lambda()
                              (setq tab-width 8)
                              (linum-mode 0)))
 
-(defun alt-shell-dwim (arg)
-  "Run an inferior shell like `shell'. If an inferior shell as its I/O
+(defun alt-term-dwim (arg)
+  "Run an inferior shell like `term'. If an inferior shell as its I/O
  through the current buffer, then pop the next buffer in `buffer-list'
  whose name is generated from the string \"*shell*\". When called with
  an argument, start a new inferior shell whose I/O will go to a buffer
@@ -12,9 +12,9 @@
   (let* ((shell-buffer-list
           (let (blist)
             (dolist (buff (buffer-list) blist)
-              (when (string-match "^\\*shell\\*" (buffer-name buff))
+              (when (string-match "^\\*terminal\\*" (buffer-name buff))
                 (setq blist (cons buff blist))))))
          (name (if arg
-                   (generate-new-buffer-name "*shell*")
+                   (generate-new-buffer-name "*terminal*")
                  (car shell-buffer-list))))
-    (shell name)))
+    (term name)))
