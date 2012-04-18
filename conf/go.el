332d5abda409d0dd45a551d8648edace798ab509
Lincoln Stoll <lstoll@lstoll.net>
2012-04-18 00:00:52 -0700

    gofmt on save, flymake for go

--- conf/go.el
+++ conf/go.el
@@ -1,5 +1,20 @@
 (add-hook 'go-mode-hook
           (lambda ()
-            (setq indent-tabs-mode t)
             (setq tab-width 4)
-            (auto-complete-mode)))
+            (auto-complete-mode)
+            (add-hook 'before-save-hook #'gofmt-before-save)))
+
+; Flymake
+(require 'flymake)
+
+(defun flymake-go-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "go" (list "build" "-o" "/dev/null" temp-file))))
+
+(push '(".+\\.go$" flymake-go-init) flymake-allowed-file-name-masks)
+
+(add-hook 'go-mode-hook 'flymake-mode)
