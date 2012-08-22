ade1e5b36c1928234cb388e0a82f113b468bbfd1
Lincoln Stoll <lstoll@lstoll.net>
2012-08-22 16:18:45 +0200

    Fix up el-get bootstrap

--- init.el
+++ init.el
@@ -19,14 +19,13 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (goto-char (point-max))
-     (eval-print-last-sexp))))
+       (with-current-buffer
+           (url-retrieve-synchronously
+            "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+         (goto-char (point-max))
+         (eval-print-last-sexp)))
 
-(el-get 'sync)
-(el-get 'sync)
+(require 'el-get)
 
 ; Actual package list - hand over, it will load the configs on a per package
 ; basis
