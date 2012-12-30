eaa588dfaf02a9d14f339461bb7091b3b67d36a0
Lincoln Stoll <lstoll@me.com>
2012-12-30 03:04:50 +0100

    Fix order issue for AC setup

--- packages.d/init-auto-complete.el
+++ packages.d/init-auto-complete.el
@@ -1,5 +1,5 @@
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (require 'auto-complete-config)
 (ac-config-default)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
