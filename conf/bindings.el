b03c84b28d266172f9bf4251061487310f5283ce
Lincoln Stoll <lstoll@me.com>
2012-12-30 02:37:04 +0100

    Fix moved auto-complete, manual add ac-nrepl.

--- conf/bindings.el
+++ conf/bindings.el
@@ -49,7 +49,7 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; Smex
-(global-set-key (kbd "M-x") 'smex)
+;;(global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
--- packages.el
+++ packages.el
@@ -2,12 +2,31 @@
 
 (setq el-get-sources
       '(
-        ;; (:name ac-nrepl
-        ;;       :description "Nrepl completion source for Emacs auto-complete package"
-        ;;       :type github
-        ;;       :pkgname "purcell/ac-nrepl"
-        ;;       :depends (auto-complete clojure-mode nrepl)
-        ;;       :features ac-nrepl)
+        ;; Manual import from the auto-complete, popup and fuzzy.rcp, the repos have moved.
+        (:name auto-complete
+               :website "http://cx4a.org/software/auto-complete/"
+               :description "The most intelligent auto-completion extension."
+               :type github
+               :pkgname "auto-complete/auto-complete"
+               :depends (popup fuzzy))
+        (:name popup
+               :website "https://github.com/m2ym/popup-el"
+               :description "Visual Popup Interface Library for Emacs"
+               :type github
+               :pkgname "auto-complete/popup-el")
+        (:name fuzzy
+               :website "https://github.com/m2ym/fuzzy-el"
+               :description "Fuzzy matching utilities for GNU Emacs"
+               :type github
+               :pkgname "auto-complete/fuzzy-el")
+
+        ;; Manual import from el-get master
+        (:name ac-nrepl
+               :description "Nrepl completion source for Emacs auto-complete package"
+              :type github
+              :pkgname "purcell/ac-nrepl"
+              :depends (auto-complete clojure-mode nrepl)
+              :features ac-nrepl)
         ))
 
 
