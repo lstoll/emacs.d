bd56380139285d03c5ea1f0cb0923508a2039613
Lincoln Stoll <lstoll@me.com>
2012-12-30 02:24:38 +0100

    Remove a bunch of old cruft
    We have version control, yo.

--- packages.el
+++ packages.el
@@ -1,89 +1,41 @@
 ;; Contains the list of packages we are using
 
 (setq el-get-sources
-      '(;;(:name ruby-mode
-        ;;       :type elpa
-        ;;       :load "ruby-mode.el"
-        ;;       :after (lambda() (load "conf/ruby")))
-        ;;(:Name textmate
-        ;;       :type git
-        ;;       :url "git://github.com/defunkt/textmate.el"
-        ;;       :load "textmate.el"
-        ;;       :after (lambda() (textmate-mode)))
-        ;;(:name clojure-mode
-        ;;       :after
-        ;;       (lambda () (add-hook 'clojure-mode-hook 'paredit-mode)))
-        ;; (:name paredit
-               ;; :url "https://raw.github.com/emacsmirror/paredit/master/paredit.el")
-        ;;(:name python
-        ;;       :after (lambda () (load "conf/python")))
-        ;;(:name ack-and-a-half
-        ;;       :type git
-        ;;       :url "https://github.com/jhelwig/ack-and-a-half.git"
-        ;;       :load "ack-and-a-half.el")
-        ;;(:name scala-mode
-        ;;       :after (lambda () (load "conf/scala")))
-        ;;(:name go-mode
-        ;;       :after (lambda () (load "conf/go")))
-        ;;(:name flymake-jslint
-        ;;       :type git
-        ;;       :url "git://github.com/purcell/flymake-jslint.git"
-        ;;       :load "flymake-jslint.el"
-        ;;       :after (lambda() (add-hook 'js-mode-hook 'flymake-jslint-load)))
-        ;;(:name flymake-coffee
-        ;;       :type git
-        ;;       :url "git://github.com/purcell/flymake-coffee.git"
-        ;;       :load "flymake-coffee.el"
-        ;;       :after (lambda() (add-hook 'coffee-mode-hook 'flymake-coffee-load)))
-        ;; (:name inf-ruby
-        ;;       :type git
-        ;;       :url "git://github.com/nonsequitur/inf-ruby.git")
-        ;;(:name ruby-electric
-        ;;      :type git
-        ;;       :url "git://github.com/qoobaa/ruby-electric.git")
-        ;; (:name nrepl
-        ;;        :description "An Emacs client for nREPL, the Clojure networked REPL server."
-        ;;        :type git
-        ;;        :url "git://github.com/kingtim/nrepl.el.git"
-        ;;        :depends clojure-mode
-        ;;        :features nrepl
-        ;;        :after (lambda () (add-hook 'nrepl-interaction-mode-hook
-        ;;                                    'nrepl-turn-on-eldoc-mode)))
+      '(
+        ;; (:name ac-nrepl
+        ;;       :description "Nrepl completion source for Emacs auto-complete package"
+        ;;       :type github
+        ;;       :pkgname "purcell/ac-nrepl"
+        ;;       :depends (auto-complete clojure-mode nrepl)
+        ;;       :features ac-nrepl)
         ))
 
 
 (setq my-packages
       (append
        '(
-         ;; ruby-mode ; built-in
          textmate
          clojure-mode
          paredit
          python
-         ;; ruby-electric
-         ;; ruby-compilation
          yaml-mode
          css-mode
          auto-complete
          ;; auto-complete-ruby
-         ;; ac-python
-         ac-slime
          markdown-mode
          coffee-mode
          flymake-ruby
          textile-mode
-         ;;haml-mode
-         ;;sass-mode
          magit
          smex
          rhtml-mode
          ack
-         ;;rinari ; fails if listed to install.
          column-marker
          nrepl
          ruby-electric
+         rainbow-delimiters
          )
-         ;magithub)
-       (mapcar 'el-get-source-name el-get-sources)))
+       (mapcar 'el-get-source-name el-get-sources)
+       ))
 
 (el-get 'sync my-packages)
