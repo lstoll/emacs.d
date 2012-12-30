869f6db6c5402ae3b5debf9373c845ad8bd92710
Lincoln Stoll <lstoll@me.com>
2012-12-30 02:52:06 +0100

    auto-complete for nrepl.

---
+++ packages.d/init-ac-nrepl.el
@@ -0,0 +1,6 @@
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode))
+
+(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
