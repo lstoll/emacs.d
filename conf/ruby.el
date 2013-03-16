46c6bc35f7cafb9894f7cc2b4c2fc3463fd931fd
Lincoln Stoll <lstoll@me.com>
2013-03-16 18:06:56 +1100

    Play with fixing ruby indentation

--- conf/ruby.el
+++ conf/ruby.el
@@ -65,3 +65,20 @@
 (add-hook 'ruby-mode-hook (lambda ()
                             (highlight-indentation-mode)
                             (highlight-indentation-current-column-mode)))
+
+;; https://gist.github.com/fujin/5173680
+(setq ruby-deep-indent-paren nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
