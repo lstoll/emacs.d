cad08a9500d182e7ca7399d0f5ea29feb1ad4bb6
Lincoln Stoll <lstoll@me.com>
2013-03-20 14:27:52 +1100

    That 'improved' indentation isn't quite what I want

--- conf/ruby.el
+++ conf/ruby.el
@@ -67,18 +67,18 @@
                             (highlight-indentation-current-column-mode)))
 
 ;; https://gist.github.com/fujin/5173680
-(setq ruby-deep-indent-paren nil)
-(defadvice ruby-indent-line (after unindent-closing-paren activate)
-  (let ((column (current-column))
-        indent offset)
-    (save-excursion
-      (back-to-indentation)
-      (let ((state (syntax-ppss)))
-        (setq offset (- column (current-column)))
-        (when (and (eq (char-after) ?\))
-                   (not (zerop (car state))))
-          (goto-char (cadr state))
-          (setq indent (current-indentation)))))
-    (when indent
-      (indent-line-to indent)
-      (when (> offset 0) (forward-char offset)))))
+;; (setq ruby-deep-indent-paren nil)
+;; (defadvice ruby-indent-line (after unindent-closing-paren activate)
+;;   (let ((column (current-column))
+;;         indent offset)
+;;     (save-excursion
+;;       (back-to-indentation)
+;;       (let ((state (syntax-ppss)))
+;;         (setq offset (- column (current-column)))
+;;         (when (and (eq (char-after) ?\))
+;;                    (not (zerop (car state))))
+;;           (goto-char (cadr state))
+;;           (setq indent (current-indentation)))))
+;;     (when indent
+;;       (indent-line-to indent)
+;;       (when (> offset 0) (forward-char offset)))))
