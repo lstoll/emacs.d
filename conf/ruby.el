417d9b8ef400d3b6d5273659aa44d3a5861fee1d
Lincoln Stoll <lstoll@lstoll.net>
2013-06-11 00:07:51 +0200

    align for ruby

--- conf/ruby.el
+++ conf/ruby.el
@@ -85,3 +85,31 @@
 
 ;; try and stop that deep paren shit
 (setq ruby-deep-indent-paren nil)
+
+;; setup align for ruby-mode
+(require 'align)
+
+(defconst align-ruby-modes '(ruby-mode)
+  "align-perl-modes is a variable defined in `align.el'.")
+
+(defconst ruby-align-rules-list
+  '((ruby-comma-delimiter
+     (regexp . ",\\(\\s-*\\)[^/ \t\n]")
+     (modes  . align-ruby-modes)
+     (repeat . t))
+    (ruby-string-after-func
+     (regexp . "^\\s-*[a-zA-Z0-9.:?_]+\\(\\s-+\\)['\"]\\w+['\"]")
+     (modes  . align-ruby-modes)
+     (repeat . t))
+    (ruby-symbol-after-func
+     (regexp . "^\\s-*[a-zA-Z0-9.:?_]+\\(\\s-+\\):\\w+")
+     (modes  . align-ruby-modes)))
+  "Alignment rules specific to the ruby mode.
+See the variable `align-rules-list' for more details.")
+
+(add-to-list 'align-perl-modes 'ruby-mode)
+(add-to-list 'align-dq-string-modes 'ruby-mode)
+(add-to-list 'align-sq-string-modes 'ruby-mode)
+(add-to-list 'align-open-comment-modes 'ruby-mode)
+(dolist (it ruby-align-rules-list)
+  (add-to-list 'align-rules-list it))
