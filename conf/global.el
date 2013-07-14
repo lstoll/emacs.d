c95b4f6c096e08ffae5f0ece4bee0d64bd066850
Lincoln Stoll <lstoll@lstoll.net>
2013-07-14 23:25:14 +0200

    adjust font, always full screen

--- conf/global.el
+++ conf/global.el
@@ -36,10 +36,10 @@
 (tooltip-mode -1)
 
 ; Decent font choice
-(if (string-match "lstoll-mini" (system-name))
-    (progn (set-frame-font "Meslo LG M DZ-10")
-           (set-frame-parameter nil 'fullscreen 'fullboth))
-    (set-frame-font "Meslo LG M DZ-12"))
+;; (if (string-match "lstoll-mini" (system-name))
+(progn (set-frame-font "Meslo LG M DZ-11")
+       (set-frame-parameter nil 'fullscreen 'fullboth))
+    ;; (set-frame-font "Meslo LG M DZ-12"))
 
 ; Theme
 (load-theme 'tango-dark)
