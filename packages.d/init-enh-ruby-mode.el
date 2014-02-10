f763d0668d89caa33920fe8402a7e254c10e84b3
Lincoln Stoll <lstoll@lstoll.de>
2014-02-10 14:41:08 +1100

    enh-ruby-mode tweaks

--- packages.d/init-enh-ruby-mode.el
+++ packages.d/init-enh-ruby-mode.el
@@ -1,1 +1,3 @@
 (setq enh-ruby-program "/usr/bin/ruby") ; because rbenv otherwise
+(setq enh-ruby-deep-indent-paren nil)
+(add-to-list 'auto-mode-alist '("\\(Gemfile\\|Puppetfile\\|Vagrantfile\\|\\.rb\\)$" . enh-ruby-mode))
