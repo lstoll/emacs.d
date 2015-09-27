f6fb4efb6af8906e9b7c9a11f3ba8efc3cd2a1f3
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-09-26 21:50:09 -0400

    save that for retina laptop experimentation

--- lisp/init-maxframe.el
+++ lisp/init-maxframe.el
@@ -2,6 +2,12 @@
 
 (add-hook 'window-setup-hook 'maximize-frame t)
 
+;;(mf-display-padding-height 46 nil nil
+;;       "The default value of 45 leaves the titlebar tucked up under"
+;;      " the menu bar on OSX Lion"
+;;       " See http://debbugs.gnu.org/cgi/bugreport.cgi?bug=10449")
+
+
 (setq mf-max-width 1600)
 
 (provide 'init-maxframe)
