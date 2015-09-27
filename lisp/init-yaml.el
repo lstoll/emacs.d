aba198e5de787d14620b0a469e142cae211a2770
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-09-27 00:17:36 -0400

    auto complete mode with yaml

--- lisp/init-yaml.el
+++ lisp/init-yaml.el
@@ -1,4 +1,9 @@
 (require-package 'yaml-mode)
 
+(defun customizations-for-yaml-mode ()
+  (auto-complete-mode)
+  )
+
+(add-hook 'yaml-mode-hook 'customizations-for-yaml-mode)
 
 (provide 'init-yaml)
